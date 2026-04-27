7288600eaf7ee151ec4b7aec6bd446f80bb10dbd
renataaferreira <ferreira.renata@escolar.ifrn.edu.br>
2026-04-27 08:29:02 -0300

    Atividade
    oi

--- dialogue4m2026/Assets/Scripts/MenuManager.cs
+++ dialogue4m2026/Assets/Scripts/MenuManager.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
 
-public class MenuController : MonoBehaviour
+public class MenuManager : MonoBehaviour
 {
     void Start()
     {
+        // Define o estado como MenuPrincipal ao entrar na cena
         GameManager.Instance.SetState(GameManager.GameState.MenuPrincipal);
+    }
+
+    // BOTÃO JOGAR
+    public void Jogar()
+    {
+        GameManager.Instance.StartGame();
+    }
+
+    // BOTÃO SAIR
+    public void Sair()
+    {
+        GameManager.Instance.QuitGame();
     }
 }
